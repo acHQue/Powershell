64da0a19e95b4af09fb842e7d5ab5220440f8154
acHQue <32178723+acHQue@users.noreply.github.com>
2019-03-14 15:53:27 +0000

    Add files via upload

---
+++ Working-RoadState.ps1
@@ -0,0 +1,35 @@
+﻿#
+# This is a small Transport For London project.
+# 20190314
+# https://api.tfl.gov.uk/
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$headers = @{
+
+'api_key' = '72602fada39738dc658ba4bc67c7d177'
+'app_id'  = '4076bf9f'
+}
+
+$road = Invoke-RestMethod -Method Get -Uri https://api.tfl.gov.uk/road -Headers $headers 
+
+for ($i = 0; $i -lt 23; $i++)
+{ 
+    
+ 
+$rdname =  $road[$i].displayName
+
+#[array]$state = $road[$i].statusSeverity
+
+#$tolower = $airquality.tolower()
+
+echo "=================================="
+
+Write-Output -InputObject ('Name: {0}' -f $rdname)
+
+Write-Output -InputObject ('Road state is: {0}' -f $road[$i].displayName)
+
+echo "=================================="
+
+ 
+}
