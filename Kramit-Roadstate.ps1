319646e7494b397ee47a66f8c8da693ec555fa98
acHQue <32178723+acHQue@users.noreply.github.com>
2019-03-14 15:58:51 +0000

    Add files via upload

---
+++ Kramit-Roadstate.ps1
@@ -0,0 +1,25 @@
+﻿# @kramit's improvements on TFL-Roadstate
+# 
+#
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$headers = @{
+
+'api_key' = '72602fada39738dc658ba4bc67c7d177'
+'app_id'  = '4076bf9f'
+}
+
+$road = Invoke-RestMethod -Method Get -Uri https://api.tfl.gov.uk/road -Headers $headers 
+
+#blank array   
+$collection = @()    
+
+for ($i = 0; $i -lt $road.Count; $i++)
+{ 
+
+$collection += New-Object psobject -Property @{Name=($road[$i].displayName);State=($road[$i].statusSeverity)}
+
+}
+
+$collection
